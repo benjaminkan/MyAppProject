25722824474549ed854ed0f4636517bd268a6d36
Benjamin Kanerva <benjamin.kanerva@hotmail.com>
2020-02-26 11:58:12 +0200

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Platform, ActionSheetController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(public actionsheetController: ActionSheetController) {}
 
-  constructor() {}
+  async openMenu() {
+    const actionSheet = await this.actionsheetController.create({
+      header: "Albums",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: "trash",
+          handler: () => {
+            console.log("Delete clicked");
+          }
+        },
 
+        {
+          text: "Share",
+          icon: "share",
+          handler: () => {
+            console.log("Share clicked");
+          }
+        },
+
+        {
+          text: "Play",
+          icon: "caret-forward-outline",
+          handler: () => {
+            console.log("Play clicked");
+          }
+        },
+
+        {
+          text: "Favorite",
+          icon: "heart",
+          handler: () => {
+            console.log("Favorite clicked");
+          }
+        },
+
+        {
+          text: "Cancel",
+          role: "cancel",
+          icon: "close",
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
 }
